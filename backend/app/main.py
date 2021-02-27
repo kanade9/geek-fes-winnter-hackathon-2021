1bba833b145da3141c0732a879992c989f5d2eab
akiFQC <yakaredori@gmail.com>
2021-02-27 09:50:14 +0000

    modify : jsonがない場合の挙動を修正 add: 時間によるフィルターを追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,8 @@ import json
 import csv
 import os
 import random
+import datetime
+
 
 
 def read_ccv_to_dict(path: str):
@@ -21,20 +23,56 @@ path_csv = os.path.join("..", 'data', 'performers_list.csv')
 dict_performers = read_ccv_to_dict(path_csv)
 n_performers = len(dict_performers)
 
+def recommedate_random(kws: list):
+    print(dict_performers.keys())
+    uids = random.sample(dict_performers.keys(), 4)
+    return uids
+
+def date_str_to_date_time(date, start_time):
+    connected_str = date + " " + start_time
+    dt = datetime.datetime.strptime(connected_str, '%Y/%m/%d %H:%M')
+    return dt
+
+def filer_by_date(recommend_infos : dict):
+    # 貪欲に時間が重なっているものを削除する
+    filtered  = []
+    time_set = set()
+    dt_now = datetime.datetime.now() 
+    for info in recommend_infos:
+        dt = date_str_to_date_time(info['date'], info['start_time'])
+        if not dt in time_set and dt >= dt_now:
+            filtered.append(info)
+            time_set.add(dt)
+    return filtered 
+
 @app.route("/")
 def hello():
     return "Hello World from Flask"
+
+
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    if request.method == 'POST':
+        data = request.data.decode('utf-8')
+        data = json.loads(data)
+        uids = recommedate_random(data['kw'])
 
 
 @app.route("/random", methods=["POST"])
 def random_choice():
     if request.method == 'POST':
         data = request.data.decode('utf-8')
-        data = json.loads(data)
-        keys = random.sample(dict_performers.keys(), 2)
+        if len(data)>0:
+            data = json.loads(data)
+        else:
+            data = {"kw":[]}
+        uids = recommedate_random(data['kw'])
         info = {}
-        for i, k in enumerate(keys):
-            info[i] = dict_performers[k] 
+        recommend = [] 
+        for i, k in enumerate(uids):
+            recommend.append( dict_performers[k] )
+        recommend = filer_by_date(recommend) # 貪欲に時間が重なっているものを削除する
+        info = { i:info for  i , info in enumerate( recommend) }  
         print(info)
         return jsonify(info)
 
@@ -42,8 +80,10 @@ def random_choice():
 def return_chokudai():
     if request.method == 'POST':
         data = request.data.decode('utf-8')
-        data = json.loads(data)
-        print(data)
+        if len(data)>0:
+            data = json.loads(data)
+        else:
+            data = {"kw":[]}
         info_chokudai = {
                 "name":"高橋 直大", 
                 "postion":"AtCoder株式会社 代表取締役社長",
@@ -53,7 +93,6 @@ def return_chokudai():
                 "twitter":"https://twitter.com/chomado" ,
             }
         chokudai_dict = { i:info_chokudai for i in range(3) }
-
         return jsonify(chokudai_dict)
 
 
