de53840b9aeca4d261e176a7f8d366115580ac79
kzmaro <maronkazu94@gmail.com>
2021-02-27 19:57:18 +0900

    backendの疎通確認

--- backend/app/main.py
+++ backend/app/main.py
@@ -60,4 +60,4 @@ def return_chokudai():
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=5001)
